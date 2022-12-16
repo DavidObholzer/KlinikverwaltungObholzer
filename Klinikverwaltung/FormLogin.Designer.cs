006b0f3a45557695fcb2960a91ae4f3d592c82a1
David Obholzer <david.obholzer@hak-kitz.ac.at>
2022-12-16 10:03:45 +0100

    SqlCommunicatoin, BCrypt and Login From

---
+++ Klinikverwaltung/FormLogin.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Klinikverwaltung
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(134, 247);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(303, 49);
+            this.btnRegister.TabIndex = 0;
+            this.btnRegister.Text = "Registrieren";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(134, 302);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(303, 49);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnRegister);
+            this.Name = "FormLogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnRegister;
+        private Button btnLogin;
+    }
+}
---
+++ Klinikverwaltung/SqlCommunication.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Klinikverwaltung
+{
+    internal class SqlCommunication
+    {
+        static SqlConnection con = new SqlConnection("server=localdb)\\MSSQLLocalDB;");
+        static SqlCommand cmd = new SqlCommand("", con);
+        static SqlDataReader dr;
+
+        static public bool checkForDatabase()
+        {
+            bool exist = false;
+            try
+            {
+                con.Open();
+                cmd.CommandText = "SELECT COUNT(*) FROM sys.databases WHERE name = 'DBKlinikverwaltungObholzer";
+
+                exist = Convert.ToInt32(cmd.ExecuteScalar()) == 1;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                if (con.State == System.Data.ConnectionState.Open)
+                    con.Close();
+                MessageBox.Show(ex.ToString());
+            }
+
+            if (exist)
+                return true;
+            else
+                return false;
+        }
+
+        static public void createDatabase()
+        {
+            if (checkForDatabase())
+            {
+                con.ConnectionString += "database=DBKlinikverwaltungObholzer";
+            }
+            else
+            {
+                try
+                {
+                    con.Open();
+                    cmd.CommandText = "create database DBKlinikverwaltungObholzer";
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    con.ConnectionString += "database=DBKlinikverwaltungObholzer";
+                }
+                catch (Exception ex)
+                {
+                    if (con.State == System.Data.ConnectionState.Open)
+                        con.Close();
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        static public void createTables()
+        {
+            con.Open();
+
+            cmd.CommandText = "CREATE TABLE TblUser([Id] INT NOT NULL PRIMARY KEY IDENTITY, " +
+                "[username] NVARCHAR (50), " +
+                "[password] NVARCHAR (100))";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "CREATE TABLE TblPatients([Id] INT NOT NULL PRIMARY KEY IDENTITY, " +
+                "[name] NVARCHAR (50), " +
+                "[lastname] NVARCHAR (50), " +
+                "[birthday] date," +
+                "[dateOfArrival] date," +
+                "[plannedLeave] date," +
+                "[notes] NVARCHAR (500))";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "CREATE TABLE TblStaff([Id] INT NOT NULL PRIMARY KEY IDENTITY, " +
+                "[name] NVARCHAR (50), " +
+                "[lastname] NVARCHAR (50), " +
+                "[birthday] date," +
+                "[monthlySalary] DECIMAL(8,2)" +
+                "[profession] NVARCHAR (50)," +
+                "[notes] NVARCHAR (500))";
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
